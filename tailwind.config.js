2fec0e0b03bd57712d82497180a05b2e273374ef
Sanjida_Nasrin_Shokhi <143315231+sanjidashokhi@users.noreply.github.com>
2023-10-07 19:29:55 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        colors: {
+            'bordercol': 'rgba(244, 246, 252, 0.20)',
+            'bordercoltwo': 'rgba(255, 255, 255, 0.05)',
+          },
+          fontFamily: {
+            'popfonts': ['Poppins','sans-serif',],
+            'manfonts': ['Manrope','sans-serif',],   
+          },
+          lineHeight: {
+            'linehi': '74px',
+            'linehitwo': '64px',
+            'linehithree': '48px',
+            'linehitfour': '56px',
+            
+          },
+          backgroundImage: {
+            'backimg': "url('/images/pointer1.png')",
+           
+          },
+          fontSize: {
+            fontss: '38px',
+           
+          }
+         
+        }
+    }
+}
